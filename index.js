eb35b83a19eff618b50d757a23da1f15b8349d6f
Charlie Provisional <charlieprovisional@MacBook-Air-de-Paula.local>
2022-10-16 12:20:39 +0200

    Feat: segundo ejercicio

---
+++ index.js
@@ -0,0 +1,14 @@
+const cumpleaños = new Date("19/03/1998");
+
+const lista_ejercicio =  [
+    "Carlos",
+    24, 
+    true,
+    new Date("19/03/1998"), 
+    {
+        "nombre": "A donde vamos a bailar esta noche",
+        "autor": "Javier Aznar ",
+        "fecha": new Date("01/01/2017"),
+        "url": "https://circulodetiza.es/libros/donde-bailar-esta-noche/"
+    }
+]
